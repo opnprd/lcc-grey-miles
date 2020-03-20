88d4e74a0bab374b17673a18f3c227ae69a95f94
patrick.lake2 <52745289+patricklake2@users.noreply.github.com>
2020-03-20 10:28:47 +0000

    Add boundary to geocoding API call

--- src/resources/geocode.js
+++ src/resources/geocode.js
@@ -2,7 +2,7 @@ export default async function geoCode(location) {
   if(!location) throw new Error('No location supplied');
   else {
     const key = '5b3ce3597851110001cf6248104657ec14464cc68a8aaaf62a878b74';
-    const response = await fetch(`https://api.openrouteservice.org/geocode/search?api_key=${key}&text=${encodeURIComponent(location)}&boundary.country=GB`);
+    const response = await fetch(`https://api.openrouteservice.org/geocode/search?api_key=${key}&text=${encodeURIComponent(location)}&boundary.circle.lon=-1.548&boundary.circle.lat=53.801&boundary.circle.radius=30&boundary.country=GB`);
     const results = await response.json();
     // Only take 1st result from the search for now - should allow user to choose from results
     const { features: [{geometry: { coordinates: [lon, lat] }}]} = results;
