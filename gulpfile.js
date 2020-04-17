09b776df00f6fa3d452dba9e31c4f76ff5dd7cd2
patrick.lake2 <52745289+patricklake2@users.noreply.github.com>
2020-04-17 16:58:33 +0100

    Retrieve latest Council Locations in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,42 @@
-const { parallel } = require('gulp');
+// const { parallel } = require('gulp');
+const axios = require('axios');
+const { writeFile } = require('fs');
+const { convertBNG } = require('./lib/convert-bng');
+const { parseCSV } = require('./lib/parse-csv');
+const { locationFilter } = require('./lib/location-filter');
+
+async function getLatestData() {
+  const pkgResponse = await axios.get('https://datamillnorth.org/api/action/package_show?id=council-land-and-building-assets');
+  let { result: { resources } } = pkgResponse.data;
+  const latest = resources.filter(el => el.format === 'csv').map(entry => {
+      entry.created = new Date(entry.created);
+      return entry;
+  }).sort((a, b) => b.created - a.created)[0];
+  const response = await axios({ method: 'get', url: latest.url, responseType: 'stream' });
+  return response.data;
+}
+
+const saveJson = (data, path) => {
+  writeFile(path, JSON.stringify(data), err => {
+    if(err) console.log(err);
+    return;
+  });
+};
 
 async function getCouncilLocations() {
-  // place code for council location download task here
-  // Get latest csv file
-  // Filter by type including office - exclude places with no location and freehold vacant or leased out
-  // Store in src/data directory
+  const dataStream = await getLatestData();
+  const data = await parseCSV(dataStream);
+  const processed = data.filter(locationFilter).map(entry => {
+    const [lon, lat] = convertBNG(entry.Easting, entry.Northing);
+    return {
+        name: entry.Name,
+        addr: entry['Address, Incl Postcode'],
+        lonlat: [lon, lat],
+    };
+  });
+  saveJson(processed, 'src/data/council-locations.json');
 }
 
 module.exports = {
-  default: parallel(getCouncilLocations),
-  getCouncilLocations,
+  default: getCouncilLocations,
 };
--- rollup.config.js
+++ rollup.config.js
@@ -63,6 +63,10 @@ export default {
           ],
           dest: `${outputDir}/vendor`,
         },
+        {
+          src: 'src/data',
+          dest: `${outputDir}/data`,
+        },
       ],
       hook: 'buildStart',
     }),
