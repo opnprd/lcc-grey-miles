c778999f7e1e494e8c27b71716fa643cbb1e9514
Patrick Lake <52745289+patricklake2@users.noreply.github.com>
2020-07-16 11:54:21 +0100

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,11 @@ const { locationFilter } = require('./lib/location-filter');
 async function getLatestData() {
   const pkgResponse = await axios.get('https://datamillnorth.org/api/action/package_show?id=council-land-and-building-assets');
   let { result: { resources } } = pkgResponse.data;
-  const latest = resources.filter(el => el.format === 'csv').map(entry => {
-      entry.created = new Date(entry.created);
-      return entry;
-  }).sort((a, b) => b.created - a.created)[0];
+  // const latest = resources.filter(el => el.format === 'csv').map(entry => {
+  //     entry.created = new Date(entry.created);
+  //     return entry;
+  // }).sort((a, b) => b.created - a.created)[0];
+  const latest = resources.filter(el => (new Date(el.created)).getFullYear() === 2019)[0];  //temp fix - force it to use the 2019 version
   const response = await axios({ method: 'get', url: latest.url, responseType: 'stream' });
   return response.data;
 }
