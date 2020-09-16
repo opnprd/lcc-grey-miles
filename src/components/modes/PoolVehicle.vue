afc751787cfa2f6c9daa4b767ca197ab5d0841d9
Patrick Lake <52745289+patricklake2@users.noreply.github.com>
2020-09-16 11:10:31 +0100

    Update text for Pool Vehicle section

--- src/components/modes/PoolVehicle.vue
+++ src/components/modes/PoolVehicle.vue
@@ -1,11 +1,19 @@
 <template>
   <section>
     <p>
+      <span class="note">We are working to develop the availability of low carbon electric pool
+        vehicles from some locations, but this facility is not yet available. When
+        it is,</span> you’ll need to complete a driver assessment before your first use
+      (contact Fleet Services at XXXX). This <span class="note">will</span> be an excellent option if you
+      need to use these vehicles regularly.
+    </p>
+    <!--
+      <p>
       We have low carbon electric pool vehicles available to book from some
       locations. You’ll need to complete a driver assessment before your first
       use (contact Fleet Services at XXXX). This is an excellent option if you
       need to use these vehicles regularly.
-    </p>
+    </p> -->
     <ul class="grid">
       <li>
         <a
@@ -25,7 +33,10 @@
       <li>
         <a>
           <h3>Book a driver assessment</h3>
-          <p>If you haven't used a pool vehicle before, you'll need to do this before your first use.</p>
+          <p>
+            If you haven't used a pool vehicle before, you'll need to do this
+            before your first use.
+          </p>
         </a>
       </li>
       <li>
